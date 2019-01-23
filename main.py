2c2eba93f4d6a791e085d3b4b11ea56198c6bbf9
kouhei <fai1.618033@gmail.com>
2019-01-23 23:38:29 +0900

    init

---
+++ main.py
@@ -0,0 +1,48 @@
+"""
+kerasでCNNやってみる
+https://qiita.com/yoyoyo_/items/0034e5e82813b05e41df
+"""
+
+"""
+GPU使用
+"""
+import tensorflow as tf
+from keras import backend as K
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True
+config.gpu_options.visible_device_list="0"
+sess = tf.Session(config=config)
+K.set_session(sess)
+
+"""
+ネットワーク定義
+まずはSequential()でインスタンスを作り、add()メソッドでlayerを追加する
+一番最初のConv2Dにはinput_shapeという引数を入れる
+これによって入力サイズを定義
+今回は２クラス分類とするので、最終層の出力はnum_classes=2
+入力サイズは64x64
+"""
+from keras.models import Sequential, Model
+from keras.layers import Dense, Dropout, Activation, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+
+num_classes = 2
+
+def Mynet():
+model = Sequential()
+model.add(Conv2D(32, (3, 3), padding='same', activation='relu', input_shape=(img_height, img_width, 3)))
+model.add(Conv2D(32, (3, 3), padding='same', activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+model.add(Dropout(0.25))
+
+model.add(Conv2D(64, (3, 3), padding='same', activation='relu'))
+model.add(Conv2D(64, (3, 3), padding='same', activation='relu'))
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Flatten())
+model.add(Dense(512, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(num_classes))
+model.add(Activation('softmax'))
+
+return model
